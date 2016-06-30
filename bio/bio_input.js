a6d180bd2cbbdb00f5bdc01d1f93fc54a661fb5f
jeesay <jjeesay@gmail.com>
2016-06-30 11:24:45 +0200

    Create bio_input.js

---
+++ bio/bio_input.js
@@ -0,0 +1,41 @@
+/*
+ *  OBIFlow: Omics and Bioinformatics visual programming workflow
+ *  Copyright (C) 2016  Jean-Christophe Taveau.
+ *
+ *  This file is part of OBIFlow
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with mowgli.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Authors:
+ * Jean-Christophe Taveau
+ */
+ 
+function Input(x) {
+    this.data = x;
+}
+
+Input.prototype.of = function(data) {
+    return new Input(data);
+}
+
+Input.prototype.wordcount = function(settings) {
+    // settings.word_length
+    // TODO
+    
+    
+    return Input.of(result);
+}
+
+ 
